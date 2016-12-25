438ed6ac5b2391b337710e5b4d43b5c761d5f3bb
Michihito Shigemura <m_shigemura@shigemk2.com>
2016-12-25 15:23:30 +0900

    Add ServiceRequest

--- src/main/scala/com/example/SmartProxy.scala
+++ src/main/scala/com/example/SmartProxy.scala
@@ -4,3 +4,9 @@ import akka.actor._
 
 object SmartProxyDriver extends CompletableApp(6) {
 }
+
+case class RequestService(service: ServiceRequest)
+
+trait ServiceRequest {
+  def requestId: String
+}
